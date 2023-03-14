1b6d7b0dfa8be99766b8d33d7d86e999f781b8b2
garima1kafle <100337024+garima1kafle@users.noreply.github.com>
2023-03-14 10:29:06 +0545

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+document.querySelector('.cross').style.display="none";
+document.querySelector('.hamburger').addEventListener("click", ()=>{
+    document.querySelector('.sidebar').classList.toggle('sidebarGo');
+
+    if(document.querySelector('.sidebar').classList.contains('sidebarGo')){
+        document.querySelector('.ham').style.display="inline";
+        document.querySelector('.cross').style.display="none";
+    }
+    else{
+        document.querySelector('.cross').style.display="inline";
+        document.querySelector('.ham').style.display="none";
+    }
+})
